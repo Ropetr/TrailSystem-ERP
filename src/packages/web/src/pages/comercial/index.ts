e9211e9eaa6f0e088b558156eefe8b18320b65ea
Rodrigo <planacacabamentos@gmail.com>
2025-12-14 08:54:13 -0300

    feat(comercial): add index.ts

---
+++ src/packages/web/src/pages/comercial/index.ts
@@ -0,0 +1,9 @@
+// =============================================
+// PLANAC ERP - Comercial Pages Export
+// =============================================
+
+export { ClientesPage } from './ClientesPage';
+export { ClienteFormPage } from './ClienteFormPage';
+export { ProdutosPage } from './ProdutosPage';
+export { ProdutoFormPage } from './ProdutoFormPage';
+export { OrcamentosPage } from './OrcamentosPage';
