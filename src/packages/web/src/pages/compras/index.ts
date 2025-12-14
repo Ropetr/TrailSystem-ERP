0b457ba92c63f5cdf3b46b8ba7f7c4d9eb7fecd6
Rodrigo <planacacabamentos@gmail.com>
2025-12-14 10:25:49 -0300

    feat(compras): adiciona index.ts

---
+++ src/packages/web/src/pages/compras/index.ts
@@ -0,0 +1,7 @@
+// =============================================
+// PLANAC ERP - Módulo Compras
+// =============================================
+
+export { PedidosCompraPage } from './PedidosCompraPage';
+export { CotacoesPage } from './CotacoesPage';
+export { FornecedoresPage } from './FornecedoresPage';
