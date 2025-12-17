00ed91a03cf1a80e9a76d00fa36ad15e4f660d74
Rodrigo <planacacabamentos@gmail.com>
2025-12-17 13:55:24 -0300

    chore: Atualizar exports do ClienteSelect

--- src/packages/web/src/components/ui/index.ts
+++ src/packages/web/src/components/ui/index.ts
@@ -5,7 +5,7 @@ export { Textarea } from "./Textarea";
 export { Select } from "./Select";
 export { SelectDropdown } from "./SelectDropdown";
 export { ClienteSelect } from "./ClienteSelect";
-export type { ClienteData, ClienteOption } from "./ClienteSelect";
+export type { ClienteOption, ClienteSelectProps } from "./ClienteSelect";
 export { DropdownMenu } from "./DropdownMenu";
 export { Card, CardHeader, CardTitle, CardContent } from "./Card";
 export { Modal } from "./Modal";
