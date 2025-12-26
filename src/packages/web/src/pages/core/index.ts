d643e8a04a8d2970c75c79a9bde4a54056a76e6a
Rodrigo <planacacabamentos@gmail.com>
2025-12-26 07:27:10 -0300

    fix: corrigir exports do módulo core

--- src/packages/web/src/pages/core/index.ts
+++ src/packages/web/src/pages/core/index.ts
@@ -1,8 +1,8 @@
-export { DashboardPage } from './DashboardPage';
-export { EmpresasPage } from './EmpresasPage';
-export { EmpresaFormPage } from './EmpresaFormPage';
-export { FiliaisPage } from './FiliaisPage';
-export { UsuariosPage } from './UsuariosPage';
-export { UsuarioFormPage } from './UsuarioFormPage';
-export { PerfisPage } from './PerfisPage';
-export { ConfiguracoesPage } from './ConfiguracoesPage';
+export { default as DashboardPage } from "./DashboardPage";
+export { default as EmpresasPage } from "./EmpresasPage";
+export { default as EmpresaFormPage } from "./EmpresaFormPage";
+export { default as FiliaisPage } from "./FiliaisPage";
+export { default as UsuariosPage } from "./UsuariosPage";
+export { default as UsuarioFormPage } from "./UsuarioFormPage";
+export { default as PerfisPage } from "./PerfisPage";
+export { default as ConfiguracoesPage } from "./ConfiguracoesPage";
